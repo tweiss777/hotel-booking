fee10edc7166f43bef3e9cc11360b60049bb403e
Tal Weiss <Tweiss747@gmail.com>
2024-06-18 16:10:48 -0400

    refactored decorator method

--- server/src/Validations/Validation.decorator.ts
+++ server/src/Validations/Validation.decorator.ts
@@ -1,18 +1,17 @@
 import { Request, Response,NextFunction } from "express";
 import SchemaMap from "./SchemaMap"
-import Ajv from "ajv";
+import AjvInstance from "./AjvInstance";
 export function ValidateSchema(value: string) {
- const func = (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+ const func = (_target: any, _propertyKey: string, descriptor: PropertyDescriptor) => {
         const originalMethod = descriptor.value;
         if(typeof originalMethod === 'function') {
-            const ajv = new Ajv();
             descriptor.value = function(req: Request, res: Response, next: NextFunction) {
                 const schema = SchemaMap.SchemaMapInstance.instance.getSchema(value);
-                const valid = ajv.validate(schema, req.body);
-                if(!valid){
+                const validate = AjvInstance.AjvInstance.instance.compile(schema);
+                if(!validate(req.body)){
                     res.status(400).send({
-                        errors: ajv.errors?.map((error) => error.message)
-                    })
+                        errors: validate.errors?.map(error => error.message)
+                    }).end()
                     return
                 }
                 originalMethod.apply(this,[req, res, next])
