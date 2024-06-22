12ef43b70f1fcb43515c5b1aa8573137b08df346
Tal Weiss <Tweiss747@gmail.com>
2024-06-21 21:30:23 -0400

    setup redux store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,34 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { Outlet } from "react-router";
-import { useAppDispatch } from "./hooks/redux.hooks";
-import { setWidth, setHeight } from "./Slices/dimension.slice";
+import React, { useEffect } from 'react';
+import './App.css';
+import { Outlet } from 'react-router';
+import { useAppDispatch } from './hooks/redux.hooks';
+import { setWidth, setHeight } from './Slices/dimension.slice';
 
 function App() {
-  const dispatch = useAppDispatch();
-  function setAppHeight() {
-    dispatch(setHeight());
-  }
+	const dispatch = useAppDispatch();
+	function setAppHeight() {
+		dispatch(setHeight());
+	}
 
-  function setAppWidth() {
-    dispatch(setWidth());
-  }
+	function setAppWidth() {
+		dispatch(setWidth());
+	}
 
-    useEffect(() => {
-        window.addEventListener("resize", setAppHeight);
-        window.addEventListener("resize", setAppWidth);
-        
-        return () => {
-            window.removeEventListener("resize", setAppHeight);
-            window.removeEventListener("resize", setAppWidth);
-        }
-    },[])
+	useEffect(() => {
+		window.addEventListener('resize', setAppHeight);
+		window.addEventListener('resize', setAppWidth);
 
+		return () => {
+			window.removeEventListener('resize', setAppHeight);
+			window.removeEventListener('resize', setAppWidth);
+		};
+	}, []);
 
-  return (
-    <div>
-      <Outlet />
-    </div>
-  );
+	return (
+		<div>
+			<Outlet />
+		</div>
+	);
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Routes/router.tsx";
-import { Provider } from "react-redux";
-import { store } from "./Store/store.ts";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/router.tsx';
+import { Provider } from 'react-redux';
+import { store } from './Store/store.ts';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>,
 );
