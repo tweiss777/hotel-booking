d965e8e70711f0e0043f5ecd3269ea00c9b7bb23
Tal Weiss <tweiss747@gmail.com>
2024-06-21 20:23:13 -0400

    setup router logic (#7)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,35 @@
+import React, { useEffect } from "react";
 import "./App.css";
+import { Outlet } from "react-router";
+import { useAppDispatch } from "./hooks/redux.hooks";
+import { setWidth, setHeight } from "./Slices/dimension.slice";
+
 function App() {
-    return (
-        <>
-            <h1>App</h1>
-        </>
-    );
+  const dispatch = useAppDispatch();
+  function setAppHeight() {
+    dispatch(setHeight());
+  }
+
+  function setAppWidth() {
+    dispatch(setWidth());
+  }
+
+    useEffect(() => {
+        window.addEventListener("resize", setAppHeight);
+        window.addEventListener("resize", setAppWidth);
+        
+        return () => {
+            window.removeEventListener("resize", setAppHeight);
+            window.removeEventListener("resize", setAppWidth);
+        }
+    },[])
+
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/router.tsx";
+import { Provider } from "react-redux";
+import { store } from "./Store/store.ts";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
-)
+);
