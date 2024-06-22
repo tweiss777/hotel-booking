8282a89aef77c42e57648f5efc6a3d5cbe7cb32e
Tal Weiss <tweiss747@gmail.com>
2024-06-21 20:23:13 -0400

    setup router logic (#7)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
+import { Outlet } from "react-router";
 function App() {
-    return (
-        <>
-            <h1>App</h1>
-        </>
-    );
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
