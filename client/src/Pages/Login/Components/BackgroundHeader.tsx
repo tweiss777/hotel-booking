d51d69aa4345f8661468f288a64e9cbbd6c25e4a
Tal Weiss <Tweiss747@gmail.com>
2024-06-23 15:11:20 -0400

    Modularized header and text

---
+++ client/src/Pages/Login/Components/BackgroundHeader.tsx
@@ -0,0 +1,7 @@
+export default function BackgroundHeader() {
+	return (
+		<div className="login-background justify-center">
+			<h2 className="login-background-header">Let your journey start here</h2>
+		</div>
+	);
+}
---
+++ client/src/Pages/Login/Components/LoginHeader.tsx
@@ -0,0 +1,7 @@
+export default function LoginHeader() {
+	return (
+		<div className="login-header">
+			<h1 style={{ color: '#FFF455' }}>Hotels.com</h1>
+		</div>
+	);
+}
