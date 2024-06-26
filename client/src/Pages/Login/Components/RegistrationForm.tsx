32c7cc63ac749fb2110b6bf9a7bec899c5ab98ad
Tal Weiss <Tweiss747@gmail.com>
2024-06-26 13:34:31 -0400

    Implemented Registration form

--- client/src/Pages/Login/Components/RegistrationForm.tsx
+++ client/src/Pages/Login/Components/RegistrationForm.tsx
@@ -1,41 +1,121 @@
 import { Card, Input, Button, Form, Alert } from 'antd';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 interface IProps {
-    loading?: boolean;
-    error?: string | null;
-    handleRegistration: (credentials: INewUser) => void;
+	loading?: boolean;
+	error?: string[] | string | null;
+	handleRegistration: (credentials: INewUser) => void;
 }
 
 interface INewUser {
-    email: string;
-    password: string;
-    confirmPassword: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
 }
 
 export default function RegistrationForm({
-    loading,
-    error,
-    handleRegistration,
+	loading,
+	error,
+	handleRegistration,
 }: IProps) {
-    const { handleSubmit, control } = useForm<INewUser>();
+	const {
+		handleSubmit,
+		control,
+		getValues,
+		formState: { errors: clientErrors },
+	} = useForm<INewUser>({
+		defaultValues: {
+			email: '',
+			password: '',
+			confirmPassword: '',
+		},
+	});
 
-    const [registrationForm, setRegistrationForm] = useState<INewUser>({
-        email: '',
-        password: '',
-        confirmPassword: '',
-    });
-    function onRegistration() {
-        handleRegistration(registrationForm);
-    }
+	function onRegistration() {
+		const values: INewUser = getValues();
+		handleRegistration(values);
+	}
 
-    return (
-        <Card bordered title="Register">
-            <Alert message={error} type="error" showIcon />
-            <Form layout="vertical" onFinish={onRegistration}>
-                `
-            </Form>
-        </Card>
-    );
+	return (
+		<Card bordered title="Register">
+			<Alert message={error} type="error" showIcon />
+			<Form layout="vertical" onFinish={handleSubmit(onRegistration)}>
+				<Controller
+					control={control}
+					name="email"
+					rules={{
+						required: true,
+						pattern: {
+							value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+							message: 'Invalid email',
+						},
+					}}
+					render={({ field }) => (
+						<>
+							<Form.Item label="Email">
+								<Input {...field} />
+							</Form.Item>
+							{clientErrors.email?.message && (
+								<p className="text-error">{clientErrors.email.message}</p>
+							)}
+						</>
+					)}
+				/>
+				<Controller
+					control={control}
+					name="password"
+					rules={{
+						required: {
+							value: true,
+							message: 'Password is required',
+						},
+					}}
+					render={({ field }) => (
+						<>
+							<Form.Item label="Password">
+								<Input.Password {...field} />
+							</Form.Item>
+							{clientErrors.password?.message && (
+								<p className="text-error">{clientErrors.password.message}</p>
+							)}
+						</>
+					)}
+				/>
+
+				<Controller
+					control={control}
+					name="confirmPassword"
+					rules={{
+						required: {
+							value: true,
+							message: 'Password is required',
+						},
+					}}
+					render={({ field }) => (
+						<>
+							<Form.Item label="Confirm Password">
+								<Input.Password {...field} />
+							</Form.Item>
+							{clientErrors.confirmPassword?.message && (
+								<p className="text-error">
+									{clientErrors.confirmPassword.message}
+								</p>
+							)}
+						</>
+					)}
+				/>
+				<Form.Item>
+					<Button
+						className="login-btn"
+						loading={loading}
+						type="primary"
+						htmlType="submit"
+					>
+						Sign up
+					</Button>
+				</Form.Item>
+				<Link to="/login">Already have an account? Login here!</Link>
+			</Form>
+		</Card>
+	);
 }
