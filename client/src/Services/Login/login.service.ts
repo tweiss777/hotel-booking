8bc18b7e4bc1d72c1e6091dc04799168edbec751
Tal Weiss <Tweiss747@gmail.com>
2024-06-26 23:13:56 -0400

    error message from server gets passed to exception

--- client/src/Services/Login/login.service.ts
+++ client/src/Services/Login/login.service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import ConfictException from '../../Errors/Conflict.exception';
 import BadRequestException from '../../Errors/BadRequest.exception';
+import ClientForbiddenException from '../../Errors/ClientForbidden.exception';
 export async function login(email: string, password: string) {
 	try {
 		const { data } = await axios.post('/api/v1/auth/login', {
@@ -10,7 +11,7 @@ export async function login(email: string, password: string) {
 		return data;
 	} catch (error: any) {
 		if (error.response && error.response.status === 401) {
-			throw new ClientForbiddenException(error.response.data.error);
+			throw new ClientForbiddenException(error.response.data.errors);
 		}
 		throw new Error('Something Went wrong');
 	}
@@ -34,7 +35,7 @@ export async function register(
 				throw new ConfictException(error.response.data.error);
 			}
 			if (error.response.status === 400) {
-				throw new BadRequestException(error.response.data.error);
+				throw new BadRequestException(error.response.data.errors);
 			}
 		}
 		throw new Error(error.response.data.error);
