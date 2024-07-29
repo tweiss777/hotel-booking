31342458011b25f771ebfc3891ee1de61343fd21
Tal Weiss <Tweiss747@gmail.com>
2024-07-29 17:54:26 -0400

    Implemented extraReducer body

--- client/src/Slices/auth.slice.ts
+++ client/src/Slices/auth.slice.ts
@@ -1,28 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit';
-
+import { loginUser, registerUser } from '../Thunks/auth.thunk';
+import { JwtPayload } from 'jwt-decode';
 type Auth = {
-    loading: boolean;
-    userInfo: object;
-    userToken: string | null;
-    error: boolean | null;
-    success: boolean;
+	loading: boolean;
+	userToken: string | null;
+	userData: JwtPayload | undefined;
+	errors: string[];
+	success: boolean;
 };
 
 const initialState: Auth = {
-    loading: false,
-    userInfo: {},
-    userToken: null,
-    error: null,
-    success: false,
+	loading: false,
+	userToken: null,
+	errors: [],
+	success: false,
+	userData: undefined,
 };
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        
-    },
+	name: 'auth',
+	initialState,
+	reducers: {},
+	extraReducers(builder) {
+		builder.addCase(loginUser.pending, (state) => {
+			state.errors = [];
+			state.loading = true;
+		});
+		builder.addCase(loginUser.fulfilled, (state, action) => {
+			state.loading = false;
+			state.success = true;
+			state.userData = action?.payload.tokenPayload;
+			state.userToken = action?.payload.token;
+		});
+		builder.addCase(loginUser.rejected, (state, action) => {
+			state.loading = false;
+			state.errors = action.payload as string[];
+            console.log(state)
+		});
+		builder.addCase(registerUser.pending, (state) => {
+			state.errors = [];
+			state.loading = true;
+		});
+		builder.addCase(registerUser.fulfilled, (state, action) => {
+			state.loading = false;
+			state.success = true;
+			state.userData = action?.payload.tokenPayload;
+			state.userToken = action?.payload.token;
+		});
+		builder.addCase(registerUser.rejected, (state, action) => {
+			state.loading = false;
+			state.errors = action.payload as string[];
+		});
+	},
 });
 
 export default authSlice.reducer;
