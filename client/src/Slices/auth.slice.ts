2e4067079a9d40e5d998e86cdf436541507279bf
Tal Weiss <tweiss747@gmail.com>
2024-07-29 18:30:47 -0400

    Authentication thunk (#13)
    * Created auth slice
    
    * created login and register thunks
    
    * added jwt-decode dependency
    
    * Refactoed thunk
    
    * Implemented extraReducer body
    
    * finished implementation. need testing
    
    * refactors to type
    
    * changed undefined to null
    
    * remove comment

---
+++ client/src/Slices/auth.slice.ts
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { loginUser, registerUser } from '../Thunks/auth.thunk';
+import { JwtPayload } from 'jwt-decode';
+type Auth = {
+	loading: boolean;
+	userToken: string | null;
+	userData: JwtPayload | null;
+	errors: string[] | null;
+	success: boolean;
+};
+
+const initialState: Auth = {
+	loading: false,
+	userToken: null,
+	errors: null,
+	success: false,
+	userData: null,
+};
+
+const authSlice = createSlice({
+	name: 'auth',
+	initialState,
+	reducers: {},
+	extraReducers(builder) {
+		builder.addCase(loginUser.pending, (state) => {
+			state.errors = null;
+			state.loading = true;
+		});
+		builder.addCase(loginUser.fulfilled, (state, action) => {
+			state.loading = false;
+			state.success = true;
+			state.userData = action?.payload.tokenPayload;
+			state.userToken = action?.payload.token;
+		});
+		builder.addCase(loginUser.rejected, (state, action) => {
+			state.loading = false;
+			state.errors = action.payload as string[];
+		});
+		builder.addCase(registerUser.pending, (state) => {
+			state.errors = null;
+			state.loading = true;
+		});
+		builder.addCase(registerUser.fulfilled, (state, action) => {
+			state.loading = false;
+			state.success = true;
+			state.userData = action?.payload.tokenPayload;
+			state.userToken = action?.payload.token;
+		});
+		builder.addCase(registerUser.rejected, (state, action) => {
+			state.loading = false;
+			state.errors = action.payload as string[];
+		});
+	},
+});
+
+export default authSlice.reducer;
---
+++ client/src/Thunks/auth.thunk.ts
@@ -0,0 +1,68 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { register, login } from '../Services/Login/login.service';
+import BadRequestException from '../Errors/BadRequest.exception';
+import UnauthorizedException from '../Errors/Unauthorized.exception';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+import ConflictException from '../Errors/Conflict.exception';
+declare module "jwt-decode" {
+    export interface JwtPayload{
+        email: string;
+        role: string;
+    }
+
+}
+
+type RegisterParams = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type Response = {
+  token: string;
+  tokenPayload: JwtPayload;
+};
+
+type LoginParams = {
+  email: string;
+  password: string;
+};
+
+
+export const registerUser = createAsyncThunk<
+  Response,
+  RegisterParams,
+  { rejectValue: string[] }
+>(
+  'api/register',
+  async ({ email, password, confirmPassword }, { rejectWithValue }) => {
+    try {
+      const { token } = await register(email, password, confirmPassword);
+      const decodedJwt = jwtDecode<JwtPayload>(token);
+      return { token, tokenPayload: decodedJwt };
+    } catch (error: any) {
+      if (error instanceof ConflictException || error instanceof BadRequestException) {
+        return rejectWithValue(error.message.split(','));
+      }
+      return rejectWithValue(['Internal server error']);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk<
+  Response,
+  LoginParams,
+  { rejectValue: string[] }
+>('api/login', async ({ email, password }, { rejectWithValue }) => {
+  try {
+    const { token } = await login(email, password);
+    const decodedJwt = jwtDecode<JwtPayload>(token);
+    return { token, tokenPayload: decodedJwt };
+  } catch (error: any) {
+    if (error instanceof UnauthorizedException) {
+      return rejectWithValue(error.message.split(','));
+    }
+    return rejectWithValue(['Internal server error']);
+  }
+});
+
