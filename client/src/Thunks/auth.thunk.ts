5befd92a00a60f2d1d732c5813015f59428f30a4
Tal Weiss <Tweiss747@gmail.com>
2024-07-29 15:55:38 -0400

    created login and register thunks

---
+++ client/src/Thunks/auth.thunk.ts
@@ -0,0 +1,71 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { register, login } from '../Services/Login/login.service';
+import BadRequestException from '../Errors/BadRequest.exception';
+import ConfictException from '../Errors/Conflict.exception';
+import UnauthorizedException from '../Errors/Unauthorized.exception';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
+declare module "jwt-decode" {
+    export interface JwtPayload{
+        email: string;
+        role: string;
+    }
+
+}
+
+
+
+type RegisterParams = {
+	email: string;
+	password: string;
+	confirmPassword: string;
+};
+
+type LoginParams = {
+	email: string;
+	password: string;
+};
+
+const registerUser = createAsyncThunk<
+	JwtPayload | undefined,
+	RegisterParams,
+	{ rejectValue: string[] }
+>(
+	'api/register',
+	async ({ email, password, confirmPassword }, { rejectWithValue }) => {
+		try {
+			const { token } = await register(email, password, confirmPassword);
+			const decodedJwt = jwtDecode(token)
+			return decodedJwt;
+		} catch (error: any) {
+			if (
+				error instanceof ConfictException ||
+				error instanceof BadRequestException
+			) {
+				rejectWithValue(error.message.split(','));
+				return;
+			}
+			rejectWithValue(['Internal server error']);
+		}
+	},
+);
+
+const loginUser = createAsyncThunk<
+	JwtPayload | undefined,
+	LoginParams,
+	{ rejectValue: string[] }
+>('api/login', async ({ email, password }, { rejectWithValue }) => {
+	try {
+		const { token } = await login(email, password);
+		const decodedJwt: JwtPayload = jwtDecode(token);
+        
+        return decodedJwt ;
+	} catch (error: any) {
+		if (error instanceof UnauthorizedException) {
+			rejectWithValue(error.message.split(','));
+			return;
+		}
+		rejectWithValue(['Internal server error']);
+	}
+});
+
+export { registerUser, loginUser };
