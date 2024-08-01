12d2ffc283cc63688888f92a3395cd395f80bb21
Tal Weiss <Tweiss747@gmail.com>
2024-08-01 16:31:01 -0400

    implemented reducer functions for mutating jwt and payload

--- client/src/Slices/auth.slice.ts
+++ client/src/Slices/auth.slice.ts
@@ -20,7 +20,14 @@ const initialState: Auth = {
 const authSlice = createSlice({
 	name: 'auth',
 	initialState,
-	reducers: {},
+	reducers: {
+        setUserToken(state, action){
+            state.userToken = action.payload.token
+        },
+        setTokenPayload(state, action){
+            state.userData = action.payload.tokenPayload
+        }
+    },
 	extraReducers(builder) {
 		builder.addCase(loginUser.pending, (state) => {
 			state.errors = null;
@@ -52,5 +59,5 @@ const authSlice = createSlice({
 		});
 	},
 });
-
+export const { setUserToken, setTokenPayload } = authSlice.actions;
 export default authSlice.reducer;
