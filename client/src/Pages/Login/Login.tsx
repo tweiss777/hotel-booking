3b7e0793918fe8cc17deda53f805ae7a0f315fba
Tal Weiss <Tweiss747@gmail.com>
2024-08-01 13:22:44 -0400

    Call thunk functions to handle login and registration

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -1,60 +1,24 @@
 import LoginForm from './Components/LoginForm';
 import './styles.scss';
 import '../../scss/grid.scss';
-import { useAppSelector } from '../../hooks/redux.hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux.hooks';
 import LoginHeader from './Components/LoginHeader';
-import { useState } from 'react';
-import { login, register } from '../../Services/Login/login.service';
 import BackgroundHeader from './Components/BackgroundHeader';
 import RegistrationForm from './Components/RegistrationForm';
 import { useLocation } from 'react-router';
-import BadRequestException from '../../Errors/BadRequest.exception';
-import ConflictException from '../../Errors/Conflict.exception';
 import { INewUser } from '../../Interfaces/INewUser';
-import UnauthorizedException from '../../Errors/Unauthorized.exception';
+import { loginUser, registerUser } from '../../Thunks/auth.thunk';
 export default function Login() {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string[]|  null>(null);
     const width = useAppSelector((state) => state.dimension.width);
     const { pathname } = useLocation();
+    const dispatch = useAppDispatch();
+    const { errors, loading } = useAppSelector((state) => state.auth);
     async function handleSubmit(loginForm: { email: string; password: string }) {
-        try {
-            if (error) setError(null);
-            setIsLoading(true);
-            const result = await login(loginForm.email, loginForm.password);
-            console.log(result);
-            // todo store the jwt in cookies or in localstorage
-        } catch (error: any) {
-            if (error instanceof UnauthorizedException) {
-                setError(error.message.split(','));
-                return;
-            }
-            setError(['Internal server error... :(']);
-        } finally {
-            setIsLoading(false);
-        }
+       dispatch(loginUser(loginForm)) 
     }
 
     async function handleRegistration(registrationForm: INewUser) {
-        try {
-            if (error) setError(null);
-            setIsLoading(true);
-            const { email, password, confirmPassword } = registrationForm;
-            const result = await register(email, password, confirmPassword);
-            console.log(result);
-            // todo store the jwt in either localstorage or cookie
-        } catch (error: any) {
-            if (
-                error instanceof BadRequestException ||
-                error instanceof ConflictException
-            ) {
-                setError(error.message.split(','));
-                return;
-            }
-            setError(['Internal server error... :(']);
-        } finally {
-            setIsLoading(false);
-        }
+        dispatch(registerUser(registrationForm)) 
     }
     return (
         <div className="login-container">
@@ -62,14 +26,14 @@ export default function Login() {
             <div className="custom-card-container">
                 {pathname.toLowerCase() === '/register' ? (
                     <RegistrationForm
-                        loading={isLoading}
-                        error={error}
+                        loading={loading}
+                        error={errors}
                         handleRegistration={handleRegistration}
                     />
                 ) : (
                     <LoginForm
-                        error={error}
-                        loading={isLoading}
+                        error={errors}
+                        loading={loading}
                         handleSubmit={handleSubmit}
                     />
                 )}
