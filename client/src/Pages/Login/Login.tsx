c7fbce66bc4452a23deb12bac2405b449edc90d9
Tal Weiss <Tweiss747@gmail.com>
2024-08-01 16:09:50 -0400

    created mock redirection

--- client/src/Pages/Login/Login.tsx
+++ client/src/Pages/Login/Login.tsx
@@ -8,17 +8,29 @@ import RegistrationForm from './Components/RegistrationForm';
 import { useLocation } from 'react-router';
 import { INewUser } from '../../Interfaces/INewUser';
 import { loginUser, registerUser } from '../../Thunks/auth.thunk';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 export default function Login() {
     const width = useAppSelector((state) => state.dimension.width);
+    const navigate = useNavigate();
     const { pathname } = useLocation();
     const dispatch = useAppDispatch();
-    const { errors, loading } = useAppSelector((state) => state.auth);
+    const { errors, loading, userToken } = useAppSelector((state) => state.auth);
+
+    useEffect(() => {
+        if(userToken !== null){
+            localStorage.setItem('token', userToken);
+            // mock redirect -> change to dashboard page
+            navigate('/');
+        }
+    }, [userToken]);
+
     async function handleSubmit(loginForm: { email: string; password: string }) {
-       dispatch(loginUser(loginForm)) 
+        dispatch(loginUser(loginForm));
     }
 
     async function handleRegistration(registrationForm: INewUser) {
-        dispatch(registerUser(registrationForm)) 
+        dispatch(registerUser(registrationForm));
     }
     return (
         <div className="login-container">
